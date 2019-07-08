70b238c5375f012f691c646254647e68feb7f14b
TetsuyaNakamura <aimizuna69@gmail.com>
2019-07-08 07:06:29 +0000

    残り9(22),10(23)

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,7 +1,7 @@
 class AttendancesController < ApplicationController
   before_action :set_user, only: :edit_one_month
   before_action :logged_in_user, only: [:update, :edit_one_month]
-  before_action :admin_or_correct_user, only: [:update, :edit_one_month, :update_one_month]
+  before_action :admin_or_correct_user, only: [:update, :edit_one_month ]
   before_action :set_one_month, only: :edit_one_month
 
   UPDATE_ERROR_MSG = "勤怠登録に失敗しました。やり直してください。"
@@ -30,7 +30,7 @@ class AttendancesController < ApplicationController
   end
 
   def update_one_month
-    ActiveRecord::Base.transaction do # トランザクションを開始します。
+   ActiveRecord::Base.transaction do # トランザクションを開始します。
       attendances_params.each do |id, item|
         attendance = Attendance.find(id)
         attendance.update_attributes!(item)
@@ -54,6 +54,8 @@ class AttendancesController < ApplicationController
 
     # 管理権限者、または現在ログインしているユーザーを許可します。
     def admin_or_correct_user
+      #@user = User.find(params[:user_id])
+      #@attendance = Attendance.find(params[:id])
       unless current_user?(@user) || current_user.admin?
         flash[:danger] = "編集権限がありません。"
         redirect_to(root_url)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
   before_action :correct_user, only: [:edit, :update]
-  before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
+  before_action :admin_user, only: [:index, :destroy, :edit_basic_info, :update_basic_info]
   before_action :set_one_month, only: :show
 
   def index
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @worked_sum = @attendances.where.not(started_at: nil).count
+      @worked_sum = @attendances.where.not(started_at: nil).count
   end
 
   def new
@@ -67,4 +67,5 @@ class UsersController < ApplicationController
     def basic_info_params
       params.require(:user).permit(:department, :basic_time, :work_time)
     end
+    
 end
